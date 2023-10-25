cd58bbaed6b07e55d2c7e5ed735f0d833e3ddc46
Temniy Daiver <orcanlend@narod.ru>
2023-10-25 11:08:05 +0300

    modified:   App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Check the number</Text>
+      <Text>Check the Number</Text>
       <StatusBar style="auto" />
     </View>
   );
