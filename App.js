b4055e7f2ef30422d0c2714cba4668c263e5ff56
Temniy Daiver <orcanlend@narod.ru>
2023-10-27 09:04:06 +0300

    modified:   App.js

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState} from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const [number1, setNumber1] = useState(0)
+  const addToNumber = () => {
+    setNumber1(number1 +1)
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Check the Number</Text>
+      
+      
+ 
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +27,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  txt:{
+    fontSize: 25,
+  },
+  txt2: {
+      fontSize: 40,
+      color: 'white',
+  },
+  button1: {
+    backgroundColor: 'blue',
+      fontSize: 40,
+      padding: 5,
+      borderRadius: 10,
+      margin: 10,
+  }
+  
 });
