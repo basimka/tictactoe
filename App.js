73ce10ac7a7c352cdd4cc48a1d9870f9d0d71670
Temniy Daiver <orcanlend@narod.ru>
2023-11-07 22:19:53 +0300

    modified:   App.js

--- App.js
+++ App.js
@@ -27,9 +27,37 @@ export default function App() {
       
 
     
-  //const checkIfPlayerWin() => {
-    //if (board[0] == board[1] )
+  const checkIfPlayerWin = () => {
+    if (board[0] == board[1] && board[1] == board[2] && board[0] !== " "){
+      playerWon(board[0])
+    }else if (board[3] == board[4] && board[4] == board[5] && board[3] !== " "){
+      playerWon(board[3])
+    }else if (board[6] == board[7] && board[7] == board[8] && board[6] !== " "){
+      playerWon(board[6])
+    }else if (board[0] == board[4] && board[4] == board[8] && board[0] !== " "){
+      playerWon(board[0])
+    }else if (board[0] == board[3] && board[3] == board[6] && board[0] !== " "){
+      playerWon(board[0])
+    }else if (board[1] == board[4] && board[4] == board[7] && board[1] !== " "){
+      playerWon(board[1])
+    }else if (board[2] == board[4] && board[4] == board[6] && board[2] !== " "){
+      playerWon(board[2])
+    }else if (board[2] == board[5] && board[5] == board[8] && board[2] !== " "){
+      playerWon(board[2])
+    }
   }
+  }
+
+  const playerWon = (Symbol) => {
+    alert("Player " + Symbol + " WON")
+    setBoard(
+      [
+        " "," "," ",
+        " "," "," ",
+        " "," "," ",
+      ])
+  }
+
   const [board, setBoard] = useState(
     [
       " "," "," ",
